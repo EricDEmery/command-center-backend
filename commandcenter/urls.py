ffc2011841dafbf24793186ccce6879f12764c21
Eric Emery <ericemery099@gmail.com>
2023-08-24 19:28:56 +0000

    update

--- commandcenter/urls.py
+++ commandcenter/urls.py
@@ -1,29 +1,12 @@
-"""
-URL configuration for api project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.urls import path, include
+from rest_framework_simplejwt import views as jwt_views
+from rest_framework import routers
 from .views import *
-from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('search/', views.UserSearch),
+    path('', include(router.urls)),
+    path('user/signup/', UserCreate.as_view(), name="create_user"),
+    path('users/<int:pk>/', UserDetail.as_view(), name="get_user_details"),
+    path('user/login/', jwt_views.TokenObtainPairView.as_view(), name='token_create'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
-
-
-from django.conf.urls.static import static
-from django.conf import settings
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
