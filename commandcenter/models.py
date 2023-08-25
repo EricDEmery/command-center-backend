42dfee3c67215ad681273da0e793f63c7ff09aad
Eric Emery <ericemery099@gmail.com>
2023-08-25 09:27:42 +0000

    update

--- commandcenter/models.py
+++ commandcenter/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
 
 class CustomUser(AbstractUser):
-    
+   
     def __str__(self):
         return self.username
 
